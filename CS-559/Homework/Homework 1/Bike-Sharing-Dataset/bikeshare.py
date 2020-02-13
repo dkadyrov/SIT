234ebdb55eef305aba3d920cd485fa4c05b432b2
Daniel Kadyrov <daniel.kadyrov@gmail.com>
2020-02-13 17:55:32 -0500

    added more to hw1

--- CS-559/Homework/Homework 1/Bike-Sharing-Dataset/bikeshare.py
+++ CS-559/Homework/Homework 1/Bike-Sharing-Dataset/bikeshare.py
@@ -2,7 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, BayesianRidge
 from sklearn.model_selection import KFold
-from sklearn import metrics
+from sklearn.metrics import mean_squared_error
 import numpy as np
 
 hour = pd.read_csv("hour.csv")
@@ -10,45 +10,31 @@ day = pd.read_csv("day.csv")
 
 cnt_hour = hour["cnt"].values.reshape(-1, 1)
 cnt_day = day["cnt"].values.reshape(-1, 1)
-day_temp = day["temp"].values.reshape(-1, 1) * 41
 
 def make_graphs():
-    plt.figure()
-    plt.scatter(hour["registered"].values.reshape(-1, 1),
-                cnt_hour, label="registered")
-    plt.scatter(hour["casual"].values.reshape(-1, 1), cnt_hour, label="casual")
-    plt.title("Hourly Users")
-    plt.legend()
-    plt.savefig("images/hour_users")
-    plt.clf()
-
     for col in range(len(hour.columns)):
         if hour.columns[col] not in ["instant", "cnt", "dteday", "registered", "casual"]:
             plt.figure()
             plt.scatter(hour[hour.columns[col]].values.reshape(-1, 1), cnt_hour)
-            plt.title("{} vs. Total Rider Hourly Count".format(hour.columns[col]))
+            plt.title("{} vs. Hourly Rider Count".format(hour.columns[col]))
             plt.xlabel("{}".format(hour.columns[col]))
-            plt.ylabel("{}".format("Total Rider Count"))
+            plt.ylabel("{}".format("Hourly Rider Count"))
             plt.savefig("images/hour_{}.png".format(hour.columns[col]))
             plt.clf()
-
-
-    plt.figure()
-    plt.scatter(hour["registered"].values.reshape(-1, 1), cnt_hour)
-    plt.scatter(hour["casual"].values.reshape(-1, 1), cnt_hour)
-    plt.savefig("images/day_users")
-    plt.clf()
 
     for col in range(len(day.columns)):
         if day.columns[col] not in ["instant", "cnt", "dteday", "registered", "casual"]:
             plt.figure()
             plt.scatter(day[day.columns[col]].values.reshape(-1, 1), cnt_day)
-            plt.title("{} vs. Total Rider Daily Count".format(day.columns[col]))
+            plt.title("{} vs. Daily Rider Count".format(day.columns[col]))
             plt.xlabel("{}".format(day.columns[col]))
-            plt.ylabel("{}".format("Total Rider Count"))
-            plt.legend()
+            plt.ylabel("{}".format("Daily Rider Count"))
             plt.savefig("images/day_{}.png".format(day.columns[col]))
             plt.clf()
+
+# make_graphs()
+
+day_temp = day["temp"].values.reshape(-1, 1) * 41
 
 crossvalidation = KFold(n_splits=10)
 linear = LinearRegression()
@@ -62,28 +48,64 @@ for train_index, test_index in crossvalidation.split(day_temp):
     linear.fit(X_train, y_train)
     ridge.fit(X_train, y_train)
     lasso.fit(X_train, y_train)
-    # bayesian.fit(X_train, y_train)
+    bayesian.fit(X_train, y_train)
     
 
 linear_pred = linear.predict(X_test)
 ridge_pred = ridge.predict(X_test)
 lasso_pred = lasso.predict(X_test)
-# bayesian_pred = bayesian.predict(X_test)
+bayesian_pred = bayesian.predict(X_test)
 
-mse_linear = metrics.mean_squared_error(y_test, linear_pred)
-mse_ridge = metrics.mean_squared_error(y_test, ridge_pred)
-mse_lasso = metrics.mean_squared_error(y_test, lasso_pred)
-# mse_bayesian = metrics.mean_squared_error(y_test, bayesian_pred)
+mse_linear = mean_squared_error(y_test, linear_pred)
+mse_ridge = mean_squared_error(y_test, ridge_pred)
+mse_lasso = mean_squared_error(y_test, lasso_pred)
+mse_bayesian = mean_squared_error(y_test, bayesian_pred)
+
+print(mse_linear)
+print(mse_ridge)
+print(mse_lasso)
+print(mse_bayesian)
 
 plt.figure()
 plt.scatter(day_temp, cnt_day)
-plt.plot(X_test, linear_pred, color="red", label=mse_linear)
-plt.plot(X_test, ridge_pred, color="purple", label=mse_ridge)
-plt.plot(X_test, lasso_pred, color="orange", label=mse_lasso)
-# plt.plot(X_test, bayesian_pred, color="green", label=mse_bayesian)
-
-plt.title("Day Temperature vs. Rider Count")
+plt.plot(X_test, linear_pred, color="red")
+plt.title("Linear: Day Temperature vs. Rider Count")
 plt.xlabel("Temperature [deg C]")
 plt.ylabel("Rider Count")
-plt.legend()
-plt.show()
+plt.savefig("images/linear.png")
+plt.clf()
+
+plt.figure()
+plt.scatter(day_temp, cnt_day)
+plt.plot(X_test, ridge_pred, color="red")
+plt.title("Ridge: Day Temperature vs. Rider Count")
+plt.xlabel("Temperature [deg C]")
+plt.ylabel("Rider Count")
+plt.savefig("images/ridge.png")
+plt.clf()
+
+plt.figure()
+plt.scatter(day_temp, cnt_day)
+plt.plot(X_test, lasso_pred, color="red")
+plt.title("Lasso: Day Temperature vs. Rider Count")
+plt.xlabel("Temperature [deg C]")
+plt.ylabel("Rider Count")
+plt.savefig("images/lasso.png")
+plt.clf()
+
+plt.figure()
+plt.scatter(day_temp, cnt_day)
+plt.plot(X_test, bayesian_pred, color="red")
+plt.title("Bayesian: Day Temperature vs. Rider Count")
+plt.xlabel("Temperature [deg C]")
+plt.ylabel("Rider Count")
+plt.savefig("images/bayesian.png")
+plt.clf()
+
+# plt.show()
+
+# plt.plot(X_test, ridge_pred, color="purple", label=mse_ridge)
+# plt.plot(X_test, lasso_pred, color="orange", label=mse_lasso)
+# plt.plot(X_test, bayesian_pred, color="green", label=mse_bayesian)
+
+
